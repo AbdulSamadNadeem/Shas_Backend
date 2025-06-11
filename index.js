14fb11e97f2477ab7175e7423b2c65328d4c76f4
AbdulSamadNadeem <samadnadeem372@gmail.com>
2025-06-12 03:47:17 +0500

    Update index.js

--- index.js
+++ index.js
@@ -5,6 +5,9 @@ const mongoose = require("mongoose");
 mongoose.connect(process.env.CONN_STR, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
+       serverSelectionTimeoutMS: 5000, 
+  socketTimeoutMS: 45000, 
+  connectTimeoutMS: 10000 
     })
     .then(() => {
       console.log("DB CONNECTED SUCCESSFULLY");
